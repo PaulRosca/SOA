9fd0b9f61fb19be4c5a9ad31868d1659bd422a6f
paulrosca <paul.rosca00@e-uvt.ro>
2023-11-11 16:36:10 +0200

    [auth] add signin function

---
+++ auth/signin/go.mod
@@ -0,0 +1,9 @@
+module handler/function
+
+go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/golang-jwt/jwt/v5 v5.1.0
+	golang.org/x/crypto v0.15.0
+)
---
+++ auth/signin/handler.go
@@ -0,0 +1,103 @@
+package function
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var db *sql.DB
+
+var jwtSecret = []byte("my_secret_key")
+
+func init() {
+	var err error
+	cfg := mysql.Config{
+		User:   "root",
+		Passwd: "test1234",
+		Net:    "tcp",
+		Addr:   "10.96.134.26:3306",
+		DBName: "auth",
+	}
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+type Credentials struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type User struct {
+	ID         int64  `json:"id"`
+	Email      string `json:"email"`
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	Password   string `json:"password"`
+}
+
+type Claims struct {
+	ID    int64  `json:"id"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	var credentials Credentials
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(&credentials)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var user User
+	row := db.QueryRow("SELECT * FROM user WHERE email = ?", credentials.Email)
+	if err := row.Scan(&user.ID, &user.Email, &user.First_name, &user.Last_name, &user.Password); err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Invalid email or password!", http.StatusUnauthorized)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(credentials.Password))
+	if err != nil {
+		http.Error(w, "Invalid email or password!", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(1 * time.Hour)
+	claims := &Claims{
+		ID:    user.ID,
+		Email: user.Email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully signed in!"))
+}
