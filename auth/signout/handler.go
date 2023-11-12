fd4c59290d7a1b47429f811353a9b2b4394f620f
paulrosca <paul.rosca00@e-uvt.ro>
2023-11-12 18:32:55 +0200

    [auth] add signout function

---
+++ auth/signout/handler.go
@@ -0,0 +1,13 @@
+package function
+
+import (
+	"net/http"
+	"time"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Expires: time.Unix(0, 0),
+	})
+}
